dce8e8e8ad75cd9b281aa60edf3acb7691c3fb70
shsim23 <shsim23@kaist.ac.kr>
2025-07-11 11:31:15 +0900

    sign in error fixed

--- express.js
+++ express.js
@@ -1,8 +1,8 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-const User = require('./models/user')
+const User = require('./models/user');
 
 const url = "mongodb+srv://tlatamus0203:3PV3ZAuEL6MrXkfr@cluster23.cyyuqox.mongodb.net/?retryWrites=true&w=majority&appName=Cluster23"
 
@@ -27,13 +27,14 @@ app.get('/', (req, res) => {
 
 app.post("/api/user/register", async (req, res) => {
   try {
-    const user = new User(req.body);
-    await user.save();
-    res.status(200).json({ success: true });
+    const user = new User(req.body)
+    await user.save()
+    console.log(user)
+    res.status(200).json({ success: true })
   } catch (err) {
-    res.json({ success: false, err });
+    res.json({ success: false, err })
   }
-});
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
