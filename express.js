207b7d3d4723c20bde4b9f6fb144445cd8cdeba9
shsim23 <shsim23@kaist.ac.kr>
2025-07-16 15:24:22 +0900

    cors policy

--- express.js
+++ express.js
@@ -30,6 +30,11 @@ app.use(cors({
   credentials: true
 }));
 
+app.options("/socket.io/*", cors({
+  origin: ["https://mario-pong.netlify.app"],
+  credentials: true
+}))
+
 app.use(session({
   secret: 'janmangjeonginyesdap', // 실제 서비스에서는 안전하게 관리
   resave: false,
@@ -368,6 +373,7 @@ const httpServer = app.listen(port, () => {
 })
 
 const io = new Server(httpServer, {
+  path: "/socket.io/",
   cors : {
     origin: "https://mario-pong.netlify.app",
     methods: ["GET", "POST"],
